37a243717197949e90a09504b763ab05b9627885
Wellington2708 <wellingtondevelopercontact@gmail.com>
2023-03-26 16:18:27 +0100

    refatorando código

--- contatos.js
+++ contatos.js
@@ -5,20 +5,23 @@
  botao.addEventListener("click", mostrarDados);
  
  function mostrarDados (){
-    document.querySelector("#dados").innerHTML = `
+    var dadosContato = document.querySelector("#dados");
+    dadosContato.innerHTML = `
     <div id="dados__contato"> 
-        <ol>
+        <ul class="cabecalho__lista__contatos">
             <li class="nome">Nome: Wellington Luis Pereira Junior</li>
             <li>Data de nascimento: 27/08/1988</li>
-            <li>Idade:34</li>
-            <li>Telefone: ********* </li>
+            <li>Idade: 34</li>
+            <li>Telefone: (+351) 938204083) </li>
             <li>Morada: Vila Nova de Famalicão - Porto</li>
-            <li class="cor__link"><i class="fa-solid fa-envelope"></i><a class="email" href="https://wellingtondevelopercontact@gmail.com">Enviar E-mail</a></li>
+            <li class="cor__link"><i class="fa-solid fa-envelope"></i><a class="email" href="mailto:wellingtondevelopercontact@gmail.com">Email</a></li>
             <li class="cor__link"><i class="fa-brands fa-linkedin"></i><a class="linkedin" href="https://www.linkedin.com/in/wellington-junior-41513874/">Home Page</a></li>  
             <li class="cor__link"><i class="fa-brands fa-github"></i><a class="gitHub" href="https://github.com/Wellington2708">Home Page</a></li>   
         
-        </ol>
-        <button onclick="fecharDados()" class="fechar" >Fechar</button>  
+        </ul>
+        <div>
+            <button onclick="fecharDados()" class="fechar" >X</button> 
+        </div> 
     </div>
     `
  }
